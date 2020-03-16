848597f3ee85cddaf0de11215b150a0eb11c4cc9
uendercarlos <44954880+uendercarlos@users.noreply.github.com>
2020-03-15 21:19:37 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Tarefas from "./components/Tarefas";
+
+function App() {
+  return (
+    <div className="App">
+      <Tarefas titulo = "To-do List com TodoLuby"/>
+    </div>
+  );
+}
+
+export default App;
